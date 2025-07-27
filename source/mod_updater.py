030233325538da8277a7303281913a10e24fe7b1
Vasyl <149047913+UnderPressurePH7@users.noreply.github.com>
2025-07-27 04:09:11 +0300

    Update mod_updater.py

--- source/mod_updater.py
+++ source/mod_updater.py
@@ -1,12 +1,19 @@
 import re
+import codecs
+import ssl
 from json import loads
-from urllib2 import urlopen
+
+try:
+    from urllib2 import urlopen
+except ImportError:
+    from urllib.request import urlopen
+
 from gui.SystemMessages import SM_TYPE
 from gui.SystemMessages import pushMessage
 from gui.Scaleform.daapi.view.lobby.hangar.Hangar import Hangar
 from helpers import getShortClientVersion
 
-FileServerConf = 'https://github.com/UnderPressurePH7/G_UA-mods/releases/download/mods_updater_4/Kudmood_updater_eu.json'
+FileServerConf = 'https://bitbucket.org/underph71/all_updaters/raw/main/MRVL_updater.json'
 
 class Data(object):
 
@@ -14,7 +21,7 @@ class Data(object):
         self.l_cfg = self.local_conf()
         self.s_cfg = self.server_conf()
         self.messageRepeat = True
-        print 'MRVL install {}'.format(self.l_cfg['LocalVer'] if self.l_cfg else 'Unknown')
+        print('MRVL install {}'.format(self.l_cfg['LocalVer'] if self.l_cfg else 'Unknown'))
 
     def comments(self, string, strip_space=True):
         tokenizer = re.compile('"|(/\\*)|(\\*/)|(//)|\n|\r')
@@ -56,64 +63,97 @@ class Data(object):
 
     def local_conf(self):
         try:
-            import codecs
             clientVersion = getShortClientVersion().replace('v.', '').strip()
             cfg_file = './mods/configs/updater.json'
             
             with codecs.open(cfg_file, mode='r', encoding='utf-8-sig') as json_file:
-                fileopen = json_file.read().decode('utf-8-sig')
+                fileopen = json_file.read() 
             return loads(self.comments(fileopen))
         except Exception as Error:
-            print 'Error local cfg:', Error
+            print('Error local cfg: {}'.format(Error))
             return None
 
     def server_conf(self):
         try:
-            import ssl
             context = ssl._create_unverified_context()
-            fileopen = urlopen(FileServerConf, context=context).read().decode('utf-8-sig')
+            response = urlopen(FileServerConf, context=context)
+            fileopen = response.read().decode('utf-8-sig')
             return loads(self.comments(fileopen))
         except Exception as Error:
-            print 'Error server cfg:', Error
+            print('Error server cfg: {}'.format(Error))
             return None
 
 def MiniClientVersion():
-    from ResMgr import openSection
-    version = openSection('../version.xml')['version'].asString
-    return version.split('v.')[1].split(' ')[0]
+    try:
+        from ResMgr import openSection
+        version = openSection('../version.xml')['version'].asString
+        return version.split('v.')[1].split(' ')[0]
+    except Exception as e:
+        print('Error getting client version: {}'.format(e))
+        return getShortClientVersion().replace('v.', '').strip()
 
-def onVehicleLoaded(self, base=Hangar._Hangar__onVehicleLoaded):
-    base(self)
-    
-    if (data.l_cfg is not None and 
-        data.s_cfg is not None and 
-        MiniClientVersion() == data.s_cfg['WotVer'] and 
-        data.messageRepeat):
+original_onVehicleLoaded = Hangar._Hangar__onVehicleLoaded
+
+def onVehicleLoaded(self):
+    try:
+        original_onVehicleLoaded(self)
+        
+        if (data.l_cfg is None or 
+            data.s_cfg is None or 
+            not data.messageRepeat):
+            return
+            
+        # Спрощена перевірка версій - не блокуємо роботу при невідповідності
+        try:
+            current_version = MiniClientVersion()
+            server_version = data.s_cfg.get('WotVer', '')
+            
+            if current_version != server_version:
+                print('Version mismatch: current={}, server={} - continuing anyway'.format(current_version, server_version))
+                # Не повертаємося, продовжуємо роботу
+        except Exception as e:
+            print('Version check error: {} - continuing anyway'.format(e))
+            # Не повертаємося, продовжуємо роботу
         
         Macros = {
-            'ServerVer': data.s_cfg['ServerVer'],
-            'LocalVer': data.l_cfg['LocalVer'],
-            'Author': data.s_cfg['Author']
+            'ServerVer': data.s_cfg.get('ServerVer', ''),
+            'LocalVer': data.l_cfg.get('LocalVer', ''),
+            'Author': data.s_cfg.get('Author', '')
         }
 
-        if data.l_cfg['LocalVer'] == data.s_cfg['ServerVer']:
-            if data.s_cfg['SystemMessages']['ActualMessages']['Enabled']:
-                txt = ''.join(data.s_cfg['SystemMessages']['ActualMessages']['Messages'])
-                txt = txt.format(**Macros)
-                pushMessage(txt, SM_TYPE.GameGreeting)
-                
-            if data.s_cfg['SystemMessages']['InfoMessages']['Enabled']:
-                txt = ''.join(data.s_cfg['SystemMessages']['InfoMessages']['Messages'])
-                txt = txt.format(**Macros)
-                pushMessage(txt, SM_TYPE.GameGreeting)
-                
-        elif data.l_cfg['LocalVer'] < data.s_cfg['ServerVer']:
-            if data.s_cfg['SystemMessages']['NewMessages']['Enabled']:
-                txt = ''.join(data.s_cfg['SystemMessages']['NewMessages']['Messages'])
-                txt = txt.format(**Macros)
-                pushMessage(txt, SM_TYPE.GameGreeting)
+        local_ver = data.l_cfg.get('LocalVer', '')
+        server_ver = data.s_cfg.get('ServerVer', '')
+        
+        if local_ver == server_ver:
+            sys_messages = data.s_cfg.get('SystemMessages', {})
+            
+            actual_msg = sys_messages.get('ActualMessages', {})
+            if actual_msg.get('Enabled', False):
+                messages = actual_msg.get('Messages', [])
+                if messages:
+                    txt = ''.join(messages).format(**Macros)
+                    pushMessage(txt, SM_TYPE.GameGreeting)
+                    
+            info_msg = sys_messages.get('InfoMessages', {})
+            if info_msg.get('Enabled', False):
+                messages = info_msg.get('Messages', [])
+                if messages:
+                    txt = ''.join(messages).format(**Macros)
+                    pushMessage(txt, SM_TYPE.GameGreeting)
+                    
+        elif local_ver < server_ver:
+            sys_messages = data.s_cfg.get('SystemMessages', {})
+            new_msg = sys_messages.get('NewMessages', {})
+            if new_msg.get('Enabled', False):
+                messages = new_msg.get('Messages', [])
+                if messages:
+                    txt = ''.join(messages).format(**Macros)
+                    pushMessage(txt, SM_TYPE.GameGreeting)
         
         data.messageRepeat = False
+        
+    except Exception as e:
+        print('Error in onVehicleLoaded: {}'.format(e))
 
 data = Data()
 
